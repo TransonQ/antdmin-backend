292dc4373e4ba5185361217ca8fd201a50c747ff
TransonQ <quanscheng@qq.com>
2024-03-11 16:42:05 +0800

    初始化接口

---
+++ app/routes/api.user.ts
@@ -0,0 +1,15 @@
+import { faker } from '@faker-js/faker'
+import { LoaderFunctionArgs, json } from '@remix-run/node'
+
+export const loader = async ({ request }: LoaderFunctionArgs) => {
+  const token = request.headers.get('Athorization')
+
+  if (!token?.includes('_ANTDMIN_')) {
+    return json({ code: '401', message: 'Unauthorized' }, 401)
+  }
+
+  return json({
+    id: faker.string.uuid(),
+    name: faker.finance.accountName(),
+  })
+}
