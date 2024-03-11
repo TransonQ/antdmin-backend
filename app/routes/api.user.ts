6b5cb8ef7c85b471b517debea44eec6d22845a90
TransonQ <quanscheng@qq.com>
2024-03-11 18:01:13 +0800

    测试 get cors

--- app/routes/api.user.ts
+++ app/routes/api.user.ts
@@ -1,20 +1,25 @@
 import { faker } from '@faker-js/faker'
 import { LoaderFunctionArgs, json } from '@remix-run/node'
+import { cors } from 'remix-utils/cors'
 
 export const loader = async ({ request }: LoaderFunctionArgs) => {
   const token = request.headers.get('Authorization')
 
-  if (!token?.includes('_ANTDMIN_')) {
-    return json(
-      { code: '401', message: 'Unauthorized' },
-      {
-        status: 401,
-      }
-    )
-  }
+  // if (!token?.includes('_ANTDMIN_')) {
+  //   return json(
+  //     { code: '401', message: 'Unauthorized' },
+  //     {
+  //       status: 401,
+  //     }
+  //   )
+  // }
 
-  return json({
-    id: faker.string.uuid(),
-    name: faker.internet.userName(),
-  })
+  return await cors(
+    request,
+    json({
+      id: faker.string.uuid(),
+      name: faker.internet.userName(),
+      token,
+    })
+  )
 }
