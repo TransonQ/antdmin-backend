82993412ecc86285e62d65bfcf28a964b19f9062
TransonQ <quanscheng@qq.com>
2024-03-11 17:34:48 +0800

    CORS

--- app/routes/api.login.ts
+++ app/routes/api.login.ts
@@ -3,7 +3,15 @@ import { ActionFunctionArgs, json } from '@remix-run/node'
 
 export const action = async ({ request }: ActionFunctionArgs) => {
   if (request.method !== 'POST')
-    return json({ code: '405', message: 'Method Not Allowed' })
+    return json(
+      { code: '405', message: 'Method Not Allowed' },
+      {
+        headers: {
+          'Access-Control-Allow-Origin': '*',
+          'Access-Control-Allow-Methods': 'GET, HEAD, POST, PUT, DELETE',
+        },
+      }
+    )
 
   await new Promise<void>((resolve) => {
     setTimeout(() => {
@@ -11,7 +19,15 @@ export const action = async ({ request }: ActionFunctionArgs) => {
     }, 1000)
   })
 
-  return json({
-    token: `_ANTDMIN_${faker.string.uuid()}`,
-  })
+  return json(
+    {
+      token: `_ANTDMIN_${faker.string.uuid()}`,
+    },
+    {
+      headers: {
+        'Access-Control-Allow-Origin': '*',
+        'Access-Control-Allow-Methods': 'GET, HEAD, POST, PUT, DELETE',
+      },
+    }
+  )
 }
