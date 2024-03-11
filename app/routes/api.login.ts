d3477d2ec0ab4f51f1638f5310909dce8c80acb4
TransonQ <quanscheng@qq.com>
2024-03-11 19:20:28 +0800

    测试login

--- app/routes/api.login.ts
+++ app/routes/api.login.ts
@@ -3,25 +3,8 @@ import { ActionFunctionArgs, json } from '@remix-run/node'
 import { cors } from 'remix-utils/cors'
 
 export const action = async ({ request }: ActionFunctionArgs) => {
-  if (request.method !== 'POST')
-    return await cors(
-      request,
-      json({ code: '405', message: 'Method Not Allowed' })
-    )
-
-  await new Promise<void>((resolve) => {
-    setTimeout(() => {
-      resolve()
-    }, 1000)
-  })
-
   return await cors(
     request,
-    json(
-      {
-        token: `_ANTDMIN_${faker.string.uuid()}`,
-      },
-      200
-    )
+    json({ token: '_ANTDMIN_' + faker.string.uuid() }, 200)
   )
 }
